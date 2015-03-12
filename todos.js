3d80fa98a40027053d166e8ae3f03b5c371e3cd3
kimyun <silverkyh1004@gmail.com>
2015-03-12 18:09:24 +0900

    1
    1

--- todos.js
+++ todos.js
@@ -1,1 +1,33 @@
 //start...
+//console.log('hello, world')
+
+function getDom(id){
+  return document.getElementById(id);
+}
+
+var todoStringField = getDom('todoString');
+
+//console.log( todoStringField , todoStringField.value );
+var form = document.getElementById('todoForm');
+
+todoStringField.addEventListener('keyup', function(event){
+//  console.log(event.keyCode);
+//  console.log( todoStringField , todoStringField.value );
+
+  if(event.keyCode === 13){
+      console.log(todoStringField.value);
+
+      var newTodo = todoStringField.value;
+      todoStringField.value= "";
+
+      var listDom = getDom('todoList');
+
+      listDom.innerHTML += '\
+        <li>\
+            <button class="delete">×</button>\
+            <input type="checkbox" class="toggle-checked">\
+            <span class="text">'+ newTodo +'</span>\
+        </li>\
+        ';
+  }
+});
