207c2012f24337cc8c0bd56d674b8eb5620124c7
kimyun <silverkyh1004@gmail.com>
2015-03-12 18:14:02 +0900

    2일차
    2일차

--- todos.js
+++ todos.js
@@ -1,1 +1,30 @@
 //start...
+//console.log("hello,world")
+
+function getDom(id){
+	return document.getElementById(id);
+};
+
+//var text = document.getElementById('todostring');
+var textField = getDom('todostring');
+var form = document.getElementById('todoform');
+
+textField.addEventListener('keyup', function(event){
+	//console.log(event);
+	//console.log(text, text.value);
+	if(event.keyCode === 13){
+		console.log(textField.value);
+
+		var newtodo = textField.value;
+		textField.value="";
+		var listDom = getDom('todoList');
+
+		listDom.innerHTML += '\
+        <li>\
+	        <button class="delete">×</button>\
+	        <input type="checkbox" class="toggle-checked">\
+	        <span class="text">'+ newtodo +'</span>\
+   		 </li>\
+		';
+	}
+});
